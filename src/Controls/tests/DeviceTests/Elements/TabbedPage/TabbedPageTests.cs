4f47bef69b668c23c164f3db6d7a1bb418237721
Shane Neuville <shneuvil@microsoft.com>
2022-11-21 10:22:22 -0600

    - add more tests and fix a couple more found issues

--- src/Controls/tests/DeviceTests/Elements/TabbedPage/TabbedPageTests.cs
+++ src/Controls/tests/DeviceTests/Elements/TabbedPage/TabbedPageTests.cs
@@ -40,22 +40,112 @@ namespace Microsoft.Maui.DeviceTests
 			});
 		}
 
-		[Fact]
-		public async Task PoppingTabbedPageDoesntCrash()
+		[Theory]
+#if ANDROID
+		[InlineData(true)]
+#endif
+		[InlineData(false)]
+		public async Task PoppingTabbedPageDoesntCrash(bool bottomTabs)
 		{
 			SetupBuilder();
 			var navPage = new NavigationPage(new ContentPage()) { Title = "App Page" };
 
 			await CreateHandlerAndAddToWindow<WindowHandlerStub>(new Window(navPage), async (handler) =>
 			{
-				await navPage.PushAsync(CreateBasicTabbedPage());
+				await navPage.PushAsync(CreateBasicTabbedPage(bottomTabs));
 				await navPage.PopAsync();
 			});
 		}
 
-		TabbedPage CreateBasicTabbedPage()
+		[Theory("Remove CurrentPage And Then Re-Add Doesnt Crash")]
+#if ANDROID
+		[InlineData(true)] // Only android has this pivot
+#endif
+		[InlineData(false)]
+		public async Task RemoveCurrentPageAndThenReAddDoesntCrash(bool bottomTabs)
 		{
-			return new TabbedPage()
+			SetupBuilder();
+
+			var tabbedPage = CreateBasicTabbedPage(bottomTabs);
+
+			var firstPage = new NavigationPage(new ContentPage());
+			tabbedPage.Children.Insert(0, firstPage);
+			tabbedPage.CurrentPage = firstPage;
+			var secondPage = tabbedPage.Children[1];
+
+			await CreateHandlerAndAddToWindow<WindowHandlerStub>(new Window(tabbedPage), async (handler) =>
+			{
+				await OnNavigatedToAsync(firstPage);
+				tabbedPage.Children.Remove(firstPage);
+				await OnNavigatedToAsync(secondPage);
+
+				// Validate that the second page becomes the current active page
+				Assert.Equal(secondPage, tabbedPage.CurrentPage);
+
+				// add the removed page back
+				tabbedPage.Children.Insert(0, firstPage);
+
+				// Validate that the second page is still the current active page
+				Assert.Equal(secondPage, tabbedPage.CurrentPage);
+
+				// Validate that we can navigate back to the first page
+				tabbedPage.CurrentPage = firstPage;
+				await OnNavigatedToAsync(firstPage);
+			});
+		}
+
+		[Theory]
+#if ANDROID
+		[InlineData(true)]
+#endif
+		[InlineData(false)]
+		public async Task SettingCurrentPageToNotBePositionZeroWorks(bool bottomTabs)
+		{
+			SetupBuilder();
+			var tabbedPage = CreateBasicTabbedPage(bottomTabs);
+			var firstPage = new NavigationPage(new ContentPage());
+			tabbedPage.Children.Insert(0, firstPage);
+			var secondPage = tabbedPage.Children[1];
+			tabbedPage.CurrentPage = secondPage;
+
+			await CreateHandlerAndAddToWindow<WindowHandlerStub>(new Window(tabbedPage), async (handler) =>
+			{
+				await OnNavigatedToAsync(secondPage);
+				Assert.Equal(tabbedPage.CurrentPage, secondPage);
+			});
+		}
+
+		[Theory]
+#if ANDROID
+		[InlineData(true)]
+#endif
+		[InlineData(false)]
+		public async Task RemovingAllPagesDoesntCrash(bool bottomTabs)
+		{
+			SetupBuilder();
+			var tabbedPage = CreateBasicTabbedPage(bottomTabs);
+			var secondPage = new NavigationPage(new ContentPage());
+			tabbedPage.Children.Add(secondPage);
+			var firstPage = tabbedPage.Children[0];
+
+			await CreateHandlerAndAddToWindow<WindowHandlerStub>(new Window(tabbedPage), async (handler) =>
+			{
+				await OnNavigatedToAsync(firstPage);
+
+				tabbedPage.Children.Remove(firstPage);
+				tabbedPage.Children.Remove(secondPage);
+
+				await OnUnloadedAsync(secondPage);
+				tabbedPage.Children.Insert(0, secondPage);
+				await OnNavigatedToAsync(secondPage);
+
+				Assert.Equal(tabbedPage.CurrentPage, secondPage);
+			});
+		}
+
+		TabbedPage CreateBasicTabbedPage(bool bottomTabs)
+		{
+			var tabs = new TabbedPage()
 			{
 				Title = "Tabbed Page",
 				Children =
@@ -63,6 +153,18 @@ namespace Microsoft.Maui.DeviceTests
 					new ContentPage() { Title = "Page 1" }
 				}
 			};
+
+			if (bottomTabs)
+			{
+				Controls.PlatformConfiguration.AndroidSpecific.TabbedPage.SetToolbarPlacement(tabs, Controls.PlatformConfiguration.AndroidSpecific.ToolbarPlacement.Bottom);
+			}
+			else
+			{
+				Controls.PlatformConfiguration.AndroidSpecific.TabbedPage.SetToolbarPlacement(tabs, 
+					Controls.PlatformConfiguration.AndroidSpecific.ToolbarPlacement.Top);
+			}
+
+			return tabs;
 		}
 	}
 }
